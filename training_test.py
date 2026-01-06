4f1824c195b3b26e0091d45a672aa8b93b49507a
YutongDu <yutong.du@aalto.fi>
2026-01-06 11:30:45 +0200

    first commit

---
+++ training_test.py
@@ -0,0 +1,14 @@
+import minigrid
+from minigrid.wrappers import ImgObsWrapper
+from stable_baselines3 import PPO
+import gymnasium as gym
+
+# env = gym.make("MiniGrid-Empty-16x16-v0", render_mode="rgb_array")
+env = gym.make("MiniGrid-Custom", render_mode="human")
+# env = gym.make("MiniGrid-Custom", render_mode="rgb_array")
+# env = gym.make("MiniGrid-Fetch-8x8-N3-v0", render_mode="human")
+
+env = ImgObsWrapper(env)
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(2e5)
